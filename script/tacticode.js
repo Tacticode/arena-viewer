963cc37a1a7cfde7f1cc2c35ef20fd91516cf4bc
helleg_f <helleg_f@git.epitech.eu>
2016-01-16 09:50:42 +0100

    demo fireball

--- script/tacticode.js
+++ script/tacticode.js
@@ -1,7 +1,7 @@
 /**
  * Tacticode - Main file
  */
-
+ 
 var Tacticode = {
     GAME_WIDTH: 1024,
     GAME_HEIGHT: 768,
@@ -9,10 +9,28 @@ var Tacticode = {
     CELL_HEIGHT_HALF: 16
 };
 
+var demo = function* (){
+	for (var i = 0; i < 10; i++)
+		yield null;
+	var c1 = Tacticode.stage.addChild(new PIXI.Sprite.fromImage("assets/test/character_dl.png"));
+	var c2 = Tacticode.stage.addChild(new PIXI.Sprite.fromImage("assets/test/character_ur.png"));
+	c1.x = 480;
+	c1.y = 130;
+	c2.x = 350;
+	c2.y = 190;
+	
+	while (true){
+		for (var i = 0; i < 60; i++)
+			yield null;
+		Tacticode.projectiles.add({x:480,y:110}, {x:320,y:180}, 20,  new PIXI.Sprite(PIXI.Texture.fromImage("assets/test/fireball.png")));
+	}
+}
+
 Tacticode.init = function () {
 	Tacticode.stage = new PIXI.Container();
 	Tacticode.renderer = new PIXI.autoDetectRenderer(1024, 768);
 	Tacticode.projectiles = new ProjectilesAnimator(Tacticode.stage);
+	Tacticode.demo = demo();
 	document.body.appendChild(Tacticode.renderer.view);
 
     requestAnimationFrame(Tacticode.animate);
@@ -22,6 +40,7 @@ Tacticode.animate = function () {
     requestAnimationFrame(Tacticode.animate);
 	Tacticode.update();
     Tacticode.renderer.render(Tacticode.stage);
+	Tacticode.demo.next();
 };
 
 /**
