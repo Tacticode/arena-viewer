f66634395434975ef01bb1bc65da19e5f678dd9e
Paul-Maxime <leduc_p@epitech.eu>
2016-08-13 21:07:01 +0200

    Change the cell information font

--- script/cellinformation.js
+++ script/cellinformation.js
@@ -10,11 +10,8 @@
  */
 Tacticode.CellInformation = function (stage) {
 	this.text = new PIXI.Text('', {
-		font : '20px Arial',
-		fill : 0xEEEEEE,
-		dropShadow : true,
-		dropShadowColor : 0x000000,
-		dropShadowDistance : 2}
+		font : '16px Consolas',
+		fill : 0x121212}
 	);
 	this.text.x = 8;
 	this.text.y = 8;
@@ -28,12 +25,12 @@ Tacticode.CellInformation.prototype.update = function (cell) {
 	if (cell) {
 		var entity = Tacticode.entities.getEntityOnCell(cell.x, cell.y, cell.z + 1);
 		var text =
-			'cell: (' + cell.x + ',' + cell.y + ') height: ' + cell.z + '\n' +
-			'accessible: ' + cell.accessible + '\n' +
+			'cell: (' + cell.x + ',' + cell.y + ') | height: ' + cell.z + '\n\n' +
+			'accessible:    ' + cell.accessible + '\n' +
 			'line-of-sight: ' + cell.los;
 		if (entity !== null) {
 			text += '\n\n';
-			text += 'id: ' + entity.id + ' health: ' + entity.health;
+			text += 'id: ' + entity.id + ' | health: ' + entity.health;
 		}
 		this.text.text = text;
 	} else {
