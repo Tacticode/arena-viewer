a8d8f1648e920150a35d6cd8a63df6c342e62dd9
Paul-Maxime <leduc_p@epitech.eu>
2016-10-07 22:06:40 +0200

    Add a gulp file to merge everything, update the index

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify')
+
+var scripts = [
+	'script/tacticode.js',
+	'script/*.js',
+	'!script/arena-viewer.js',
+	'!script/config*.js'
+];
+
+gulp.task('default', function() {
+  return gulp.src(scripts)
+      .pipe(concat('arena-viewer.js'))
+      .pipe(gulp.dest('script/'));
+});
