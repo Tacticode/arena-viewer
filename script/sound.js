7726b88e56d8218888fb410c6cc45773a573a324
Paul-Maxime <github@paulmaxime.fr>
2016-11-08 19:49:13 +0100

    Document the sounds classes

--- script/sound.js
+++ script/sound.js
@@ -1,22 +1,43 @@
 'use strict';
 
+/**
+ * Creates a new SoundManager, used to register and play sound effects.
+ * @constructor
+ * @param {string} basePath Path concatenated to individual filenames.
+ */
 var SoundManager = function (basePath) {
 	this.basePath = basePath;
 	this.sounds = {};
 	this.isMuted = false;
 };
 
+/**
+ * Registers the specified audio element, so that it can be played later.
+ * @param {string} name Name of the sound effect, used by the play() function.
+ * @param {string} path Path to the audio file. Will be concatenated with the base path.
+ * @param {number} number Amount of audio elements to register. This allows the sound to be played more than once at the same time.
+ */
 SoundManager.prototype.register = function (name, path, number) {
 	number = (number !== undefined) ? number : 1;
 	this.sounds[name] = new Sound(this.basePath + path, number);
 };
 
+/**
+ * Plays the previously registered sound effect.
+ * @param {string} name Name of the audio sound, as specified in the register() function.
+ */
 SoundManager.prototype.play = function (name) {
 	if (!this.isMuted) {
 		this.sounds[name].play();
 	}
 };
 
+/**
+ * Represents a sound effect. Will register as many Audio element as required.
+ * @constructor
+ * @param {string} path Path to the audio file.
+ * @param {number} number Amount of audio elements to register.
+ */
 var Sound = function (path, number) {
 	this.audios = [];
 	for (var i = 0; i < number; ++i) {
@@ -25,6 +46,9 @@ var Sound = function (path, number) {
 	}
 };
 
+/**
+ * Plays this sound effect using the first available audio element.
+ */
 Sound.prototype.play = function () {
 	for (var i = 0; i < this.audios.length; ++i) {
 		if (this.audios[i].paused) {
