040f55fa4b087bde6c8125581f3400211a0748a8
Paul-Maxime <github@paulmaxime.fr>
2016-11-08 19:36:50 +0100

    Document the Overlay classes

--- script/overlay.js
+++ script/overlay.js
@@ -4,6 +4,11 @@
 
 "use strict";
 
+/**
+ * Creates a new OverlayManager, used to display damage and heal values.
+ * @constructor
+ * @param stage Pixi.js stage where the overlay will be displayed.
+ */
 Tacticode.OverlayManager = function (stage) {
 	this.container = new PIXI.Container();
 	this.overlays = [];
@@ -11,6 +16,9 @@ Tacticode.OverlayManager = function (stage) {
 	stage.addChild(this.container);
 };
 
+/**
+ * Updates the existing labels, must be called once per frame.
+ */
 Tacticode.OverlayManager.prototype.update = function () {
 	var overlays = this.overlays.slice();
 	for (let i = 0; i < overlays.length; ++i) {
@@ -26,6 +34,12 @@ Tacticode.OverlayManager.prototype.update = function () {
 	}
 };
 
+/**
+ * Creates a damage label (red) at the specified location.
+ * @param {number} x The X coordinate of the new damage label.
+ * @param {number} y The Y coordinate of the new damage label.
+ * @param {number} health The value displayed on the label.
+ */
 Tacticode.OverlayManager.prototype.addDamage = function (x, y, health) {
 	x += Tacticode.GAME_WIDTH / 2;
 	y += Tacticode.GAME_HEIGHT / 4 - Tacticode.CELL_HEIGHT_HALF * 2;
@@ -33,6 +47,12 @@ Tacticode.OverlayManager.prototype.addDamage = function (x, y, health) {
 	this.overlays.push(overlay);
 };
 
+/**
+ * Creates a healing label (green) at the specified location.
+ * @param {number} x The X coordinate of the new healing label.
+ * @param {number} y The Y coordinate of the new healing label.
+ * @param {number} health The value displayed on the label.
+ */
 Tacticode.OverlayManager.prototype.addHeal = function (x, y, health) {
 	x += Tacticode.GAME_WIDTH / 2;
 	y += Tacticode.GAME_HEIGHT / 4 - Tacticode.CELL_HEIGHT_HALF * 2;
@@ -40,6 +60,15 @@ Tacticode.OverlayManager.prototype.addHeal = function (x, y, health) {
 	this.overlays.push(overlay);
 };
 
+/**
+ * Creates a new overlay label, displaying the specified text at a specified position.
+ * @constructor
+ * @param container Pixi.js container to which the text will be added.
+ * @param {number} x X coordinate of the new label.
+ * @param {number} y Y coordinate of the new label.
+ * @param {number} color Color of the new label, as an RGB integer.
+ * @param {number} text Content of the new overlay text.
+ */
 Tacticode.TextOverlay = function (container, x, y, color, text) {
 	this.container = container;
 	this.text = new PIXI.Text(text, {
@@ -58,10 +87,16 @@ Tacticode.TextOverlay = function (container, x, y, color, text) {
 	this.container.addChild(this.text);
 };
 
+/**
+ * Removes this text from the container, destroying it.
+ */
 Tacticode.TextOverlay.prototype.destroy = function () {
 	this.container.removeChild(this.text);
 };
 
+/**
+ * Moves and rescales the label based on time and speed. Must be called once per frame.
+ */
 Tacticode.TextOverlay.prototype.update = function () {
 	this.age += Tacticode.speed;
 
@@ -70,6 +105,9 @@ Tacticode.TextOverlay.prototype.update = function () {
 	this.text.scale.set(this.scale, this.scale);
 };
 
+/**
+ * @return true if the text should still be displayed, false if it should be destroyed.
+ */
 Tacticode.TextOverlay.prototype.isAlive = function () {
 	return this.age <= this.lifespan;
 };
